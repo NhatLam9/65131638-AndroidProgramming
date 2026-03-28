385b2807fba4da8cb0f8fe737a9c7488dd589305
Võ Nhật Lâm <vonhatlam1805@gmail.com>
2026-03-28 13:16:17 +0700

    BT7_ListView_HoanThien

---
+++ BT7_ListView_TinhThanh/app/src/main/java/com/example/bt7_listview_tinhthanh/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.bt7_listview_tinhthanh;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayList<String> dsTenTinhThanhVN;
+        dsTenTinhThanhVN = new ArrayList<String>();
+        dsTenTinhThanhVN.add("Hà Nội");
+        dsTenTinhThanhVN.add("Thành phố Hồ Chí Minh");
+        dsTenTinhThanhVN.add("Đồng Nai");
+        dsTenTinhThanhVN.add("Bình Thuận");
+        dsTenTinhThanhVN.add("Ninh Thuận");
+        dsTenTinhThanhVN.add("Nha Trang");
+
+        ArrayAdapter<String> adapterTinhThanh;
+        adapterTinhThanh = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dsTenTinhThanhVN);
+
+        ListView lvTenTinhThanh = findViewById(R.id.lvDanhSachTT);
+
+        lvTenTinhThanh.setAdapter(adapterTinhThanh);
+    }
+}
